b9c6d23f56f2541d2ec9cf17568aa4d0733e28b1
DirectMJ <138039347+DirectMJ@users.noreply.github.com>
2024-10-17 03:02:26 +0800

    Add files via upload

--- 016midAct1_Pattaguan.sql
+++ 016midAct1_Pattaguan.sql
@@ -24,16 +24,14 @@ begin
 end
 $$
 
-select fnGetfullname('10003');
+select fnGetfullname('10003') as FullName
 
--- next shit --
--- next shit--
--- next shit--
--- next shit--
+-- next --
+
 
 
 DELIMITER $$ 
-CREATE FUNCTION shit(emp_id VARCHAR(5))
+CREATE FUNCTION getEmpType(emp_id VARCHAR(5))
 RETURNS VARCHAR(10)
 DETERMINISTIC
 BEGIN
@@ -53,12 +51,9 @@ BEGIN
 END;
 delimiter
 $$
-select shit('10004') as EmployeeType
+select getEmpType('10004') as EmployeeType
 
--- next shit--
--- next shit--
--- next shit--
--- next shit--
+-- next --
 
 
 
@@ -105,12 +100,147 @@ FROM
     EMPLOYEE e
     INNER JOIN SALARIEDEMPLOYEE s ON e.EmpID = s.EmpID;
     
-    -- next shit --
-	-- next shit --
-	-- next shit --
-	-- next shit --
+    -- next --
 
 -- philhealth contribution--
+
+DELIMITER //
+
+CREATE FUNCTION philhealthContri(p_EmpID VARCHAR(5))
+RETURNS DECIMAL(18,2)
+DETERMINISTIC
+BEGIN
+    DECLARE v_grossSalary DECIMAL(18,2);
+    DECLARE v_philHealth DECIMAL(18,2);
+    
+  
+    IF EXISTS (SELECT 1 FROM HOURLYEMPLOYEE WHERE EmpID = p_EmpID) THEN
+        SET v_grossSalary = grossSalary(p_EmpID);
+    ELSEIF EXISTS (SELECT 1 FROM SALARIEDEMPLOYEE WHERE EmpID = p_EmpID) THEN
+       
+        SELECT (s.BasicSalary + s.Incentive) INTO v_grossSalary
+        FROM SALARIEDEMPLOYEE s
+        WHERE s.EmpID = p_EmpID;
+    ELSE
+       
+        SET v_grossSalary = 0;
+    END IF;
+
+   
+    IF v_grossSalary <= 10000 THEN
+        SET v_philHealth = v_grossSalary * 0.05;  
+    ELSEIF v_grossSalary > 10000 AND v_grossSalary <= 99999.99 THEN
+        SET v_philHealth = v_grossSalary * 0.06;  
+    ELSE
+        SET v_philHealth = v_grossSalary * 0.07;  
+    END IF;
+
+    RETURN v_philHealth;
+END //
+
+DELIMITER ;
+
+SELECT 
+    e.EmpID,
+    CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
+    philhealthContri(e.EmpID) AS PhilHealthContribution
+FROM 
+    EMPLOYEE e;
+
+-- next --
+-- PAGIBIG CONRTIBUTION --
+
+DELIMITER //
+
+CREATE FUNCTION pagibigContribution(p_EmpID VARCHAR(5))
+RETURNS DECIMAL(18,2)
+DETERMINISTIC
+BEGIN
+    DECLARE v_grossSalary DECIMAL(18,2);
+    DECLARE v_errate DECIMAL(18,2) DEFAULT 0.02; 
+    DECLARE v_pagibig DECIMAL(18,2);
+
+   
+    SET v_grossSalary = grossSalary(p_EmpID);
+    
+
+    IF v_grossSalary <= 5000 THEN
+        SET v_pagibig = (v_grossSalary * v_errate) + (v_grossSalary * v_errate); 
+    ELSE
+        SET v_pagibig = 200;  
+    END IF;
+
+    RETURN v_pagibig;
+END //
+
+DELIMITER ;
+SELECT 
+    e.EmpID,
+    CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
+    pagibigContribution(e.EmpID) AS PagibigContribution
+FROM 
+    EMPLOYEE e;
+
+-- sss --
+DELIMITER $$
+
+CREATE FUNCTION calculateSSSContribution(p_EmpID VARCHAR(5))
+RETURNS DECIMAL(18,2)
+DETERMINISTIC
+BEGIN
+    DECLARE v_grossSalary DECIMAL(18,2);
+    DECLARE v_erss DECIMAL(18,2);
+    DECLARE v_erec DECIMAL(18,2);
+    DECLARE v_eess DECIMAL(18,2);
+    DECLARE v_pfer DECIMAL(18,2);
+    DECLARE v_pfee DECIMAL(18,2);
+    DECLARE v_sssContribution DECIMAL(18,2);
+
+   
+    IF EXISTS (SELECT 1 FROM HOURLYEMPLOYEE WHERE EmpID = p_EmpID) THEN
+        SET v_grossSalary = grossSalary(p_EmpID);
+    ELSEIF EXISTS (SELECT 1 FROM SALARIEDEMPLOYEE WHERE EmpID = p_EmpID) THEN
+        SELECT (s.BasicSalary + s.Incentive) INTO v_grossSalary
+        FROM SALARIEDEMPLOYEE s
+        WHERE s.EmpID = p_EmpID;
+    ELSE
+        SET v_grossSalary = 0;
+    END IF;
+
+    
+    SELECT ERSS, EREC, EESS, PFER, PFEE
+    INTO v_erss, v_erec, v_eess, v_pfer, v_pfee
+    FROM SSS  
+    WHERE v_grossSalary >= SalaryMin
+	AND (SalaryMax IS NULL OR v_grossSalary <= SalaryMax)
+    LIMIT 1; 
+
+  
+    SET v_sssContribution = v_erss + v_erec + v_eess + v_pfer + v_pfee;
+
+    RETURN v_sssContribution;
+END $$
+
+DELIMITER ;
+SELECT 
+    e.EmpID,
+    CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
+    calculateSSSContribution(e.EmpID) AS SSSContribution
+FROM 
+    EMPLOYEE e;
+
+
+
+
+
+
+
+
+
+
+
+    
+    
 
 
 
@@ -125,4 +255,3 @@ FROM
 
 
  
-
